9b71641632fe488026386d5b8c984d3617f74e6d
Stuart Urback <stuart@urback.net>
2016-01-04 10:08:25 -0700

    Made object method prototype

--- planetary-warmup/app.js
+++ planetary-warmup/app.js
@@ -1,12 +1,12 @@
 var object = require('./weekday_info.json')
 
-function planetCorrespondence(day){
+Object.prototype.planetCorrespondence = function(day){
   day = day.toLowerCase();
   day = day.charAt(0).toUpperCase() + day.slice(1);
-  return (day in object) ? object[day].planet : "Please input a day";
+  return (day in this) ? this[day].planet : "Please input a day";
 }
 
-console.log(planetCorrespondence("DAY"))
-console.log(planetCorrespondence("monday"))
-console.log(planetCorrespondence("tuESDAY"))
-console.log(planetCorrespondence("WEDNESDAY"))
+console.log(object.planetCorrespondence("DAY"))
+console.log(object.planetCorrespondence("monday"))
+console.log(object.planetCorrespondence("tuESDAY"))
+console.log(object.planetCorrespondence("WEDNESDAY"))
