9027c4f23d76ca729ca7f234353dc2e5d48412d7
Stuart Urback <stuart@urback.net>
2016-01-12 09:42:26 -0700

    Complete first test

--- sqrtapprox/sqrtapprox.js
+++ sqrtapprox/sqrtapprox.js
@@ -1,6 +1,6 @@
-// We want to approximate the sqrt function. Usually this functions takes a floating point number and returns another floating point number, but in this kata we're going to work with integral numbers instead.
-//
-// Your task is to write a function that takes an integer n and returns either
-//
-// an integer k if n is a square number, such that k * k == n or
-// a range (k, k+1), such that k * k < n and n < (k+1) * (k+1).
+module.exports = {
+  isASquare: function(integer){
+    return Math.sqrt(integer) % 1  === 0;
+    // return "Bing Bong";
+  }
+}
