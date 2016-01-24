0d576db451592721f578e2d4482fc4e1a5dd1b7f
Stuwert <stuart@urback.net>
2016-01-23 21:51:20 -0700

    Complete refactor

--- weirdcase/app.js
+++ weirdcase/app.js
@@ -1,11 +1,9 @@
 function toWeirdCase(string){
-  var newStringArray = string.split(' ');
-  newStringArray = newStringArray.map(function(item){
+  return string.split(' ').map(function(item){
     return item.split('').map(function(item, i){
       return i % 2 === 0 ? item.toUpperCase() : item.toLowerCase()
     }).join('');
-  })
-  return newStringArray.join(' ')
+  }).join(' ');
 }
 
 console.log(toWeirdCase("String"));
