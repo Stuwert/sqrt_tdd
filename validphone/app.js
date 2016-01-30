ef326b2d23fc265cec351ec4822243102b050970
Stuart Urback <stuart@urback.net>
2016-01-30 11:19:22 -0700

    Update repo

--- validphone/app.js
+++
@@ -1,8 +0,0 @@
-function validPhoneNumber(phoneNumber){
-  //TODO: Return whether phoneNumber is in the proper form
-  var regex = /\d\d\d/
-  return phoneNumber.match(regex) === -1 ? false : true;
-
-}
-
-console.log(validPhoneNumber('33'));
